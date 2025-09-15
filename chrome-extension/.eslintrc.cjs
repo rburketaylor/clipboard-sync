e124c95314fb0f247fbb979a18a43b5670753160
Burke T <54682710+rburketaylor@users.noreply.github.com>
2025-09-15 19:24:20 -0300

    chore(extension): add ESLint config for Vue/TS, fix lint errors; pin ESLint v8; add eslint-config-prettier; add CJS mock backend

--- chrome-extension/.eslintrc.cjs
+++ chrome-extension/.eslintrc.cjs
@@ -1,8 +1,13 @@
 module.exports = {
   root: true,
   env: { browser: true, es2022: true, node: true },
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    extraFileExtensions: ['.vue']
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -11,8 +16,10 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint', 'vue'],
   ignorePatterns: ['dist', 'node_modules'],
+  globals: { chrome: 'readonly' },
   rules: {
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }]
   }
 };
-
